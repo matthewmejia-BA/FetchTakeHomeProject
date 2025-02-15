06f8f00bc376feb8827012201404b172317f7725
matthewmejia-BA <96089468+matthewmejia-BA@users.noreply.github.com>
2025-02-15 15:13:52 -0700

    Initial Commit

---
+++ FetchTakeHomeProject/FetchTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeProjectApp.swift
+//  FetchTakeHomeProject
+//
+//  Created by BrilyanteApps on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
